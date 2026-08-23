4cf9b24ed2707b4ee7fbe0c5dc4f3bf124972161
mohitdamke <mohitdamke1@gmail.com>
2026-08-23 16:15:02 +0530

    added app apk 1.2 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.mohit.videoskipper"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
